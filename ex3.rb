510f1c8e3cc689148e6081a6d7b316f0ac75df6e
Bruno Bueno <brunopb@brunopb-laptop.(none)>
2013-05-03 15:34:04 -0300

    Ex3 refactored

--- ex3.rb
+++ ex3.rb
@@ -1,20 +1,17 @@
 def combine_anagrams(words)
-	all_anagrams = Array.new()
-	diff_sorted_words = Array.new()
+	all_anagrams = {}
 	words.each do |palavra|
-		sorted_word = palavra.downcase.each_char.to_a.sort
-		if diff_sorted_words.include?(sorted_word)
-			all_anagrams[diff_sorted_words.index(sorted_word)].push(palavra)
-		else
-			diff_sorted_words.push(sorted_word)
-			all_anagrams.push(palavra.to_a)
-		end
+		sorted_word = para_vetor_chars_ordenado_downcase(palavra)
+		all_anagrams[sorted_word] ||= []
+		all_anagrams[sorted_word].push(palavra)
 	end
-	all_anagrams.each  {|a| puts a; puts ""}
-	return all_anagrams
+	return all_anagrams.each_value.to_a
 end
 
+def para_vetor_chars_ordenado_downcase(string)
+	string.downcase.each_char.to_a.sort
+end
 
 input = ['cars', 'for', 'potatoes', 'racs', 'four','scar', 'creams',
 'scream']
-combine_anagrams(input)
+puts combine_anagrams(input).inspect
