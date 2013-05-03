ae1ba88ee5465e365b6a6c7f17243a6c2e844985
Bruno Bueno <brunopb@brunopb-laptop.(none)>
2013-05-03 14:51:09 -0300

    Ex2 refactores

--- ex2.rb
+++ ex2.rb
@@ -9,26 +9,33 @@ def rps_game_winner(game)
   raise NoSuchStrategyError unless jogada_valida(game)
 
   if( (player1[1]=="R" && player2[1]=="P") || 
-    (player1[1]=="P" && player2[1]=="S") || 
-    (player1[1]=="S" && player2[1]=="R") )
+      (player1[1]=="P" && player2[1]=="S") || 
+      (player1[1]=="S" && player2[1]=="R") )
     return player2
   end
   return player1
 end
 
 def jogada_valida(game)
-  ((game[0][1] == "R" || game[0][1] == "P" || game[0][1] == "S") && 
-   (game[1][1] == "R" || game[1][1] == "P" || game[1][1] == "S"))
+  jogada_player1 = game[0][1]
+  jogada_player2 = game[1][1]
+
+  ((jogada_player1 == "R" || jogada_player1 == "P" || jogada_player1 == "S") && 
+   (jogada_player2 == "R" || jogada_player2 == "P" || jogada_player2 == "S"))
 end
 
 def rps_tournament_winner(torneio)
-  if(torneio[0][0].class == Array)
+  if(mais_que_duas_jogadas?(torneio))
     torneio[0] = rps_tournament_winner(torneio[0])
   end
-  if(torneio[1][0].class == Array)
+  if(mais_que_duas_jogadas?(torneio))
     torneio[1] = rps_tournament_winner(torneio[1])
   end
   rps_game_winner(torneio)
+end
+
+def mais_que_duas_jogadas?(torneio)
+  (torneio[0][0].class == Array) || (torneio[1][0].class == Array)
 end
 
 jogo = [
